d785fd730cdbc21f3655435ee421b9f682364d2b
Cristopher Bazan <cris.hbn17@gmail.com>
2025-06-07 19:59:27 -0500

    apis metodopago

--- src/main/java/com/alexander/sistema_cerro_verde_backend/repository/ventas/MetodoPagoRepository.java
+++ src/main/java/com/alexander/sistema_cerro_verde_backend/repository/ventas/MetodoPagoRepository.java
@@ -1,14 +1,13 @@
 package com.alexander.sistema_cerro_verde_backend.repository.ventas;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.alexander.sistema_cerro_verde_backend.entity.ventas.MetodosPago;
 
 public interface MetodoPagoRepository extends JpaRepository<MetodosPago, Integer> {
 
-    @Query("SELECT m FROM MetodosPago m WHERE m.estado = 1")
-    List<MetodosPago> findActive();
+    @Query("SELECT COUNT(v) FROM VentaMetodoPago v WHERE v.metodoPago.idMetodoPago = :id")
+    int countVentasByMetodoPagoId(@Param("id") Integer id);
 }
