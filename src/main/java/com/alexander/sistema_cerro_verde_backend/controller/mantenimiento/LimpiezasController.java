d0535ba1387ad65a7b82a730fa988d71a1c981bb
PoolBryan <b.arbildo@unsm.edu.pe>
2025-06-11 16:17:05 -0500

    Poolcito

--- src/main/java/com/alexander/sistema_cerro_verde_backend/controller/mantenimiento/LimpiezasController.java
+++ src/main/java/com/alexander/sistema_cerro_verde_backend/controller/mantenimiento/LimpiezasController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,7 @@ import com.alexander.sistema_cerro_verde_backend.entity.mantenimiento.Limpiezas;
 import com.alexander.sistema_cerro_verde_backend.service.mantenimiento.jpa.LimpiezasService;
 
 
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/cerro-verde/limpiezas")
 public class LimpiezasController {
