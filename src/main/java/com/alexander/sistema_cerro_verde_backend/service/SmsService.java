28cbbf9b9033bf61cb558a4a98269e2c6fbc8b74
furawa23 <fabrizzioflores2014@gmail.com>
2025-06-25 07:50:55 -0500

    smsservice modificado

--- src/main/java/com/alexander/sistema_cerro_verde_backend/service/SmsService.java
+++ src/main/java/com/alexander/sistema_cerro_verde_backend/service/SmsService.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class SmsService {
   // Find your Account Sid and Token at twilio.com/console
-  public static final String ACCOUNT_SID = "ACb3822a105764f288d04edb8f9befa7a3";
-  public static final String AUTH_TOKEN = "0505342574a7df684b4834f53f9e665d";
+  public static final String ACCOUNT_SID = "[SID]";
+  public static final String AUTH_TOKEN = "[Token]";
   public static final PhoneNumber NUMBER = new PhoneNumber("+51925057443");
   public void enviarSms(String mensaje) {
     Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
